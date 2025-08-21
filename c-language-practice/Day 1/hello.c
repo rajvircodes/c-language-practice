0843c7e81ed6dceed54210d7cdfcfd4b09f309cc
rajvircodes <rajvirsinhgohil004@gmail.com>
2025-08-21 20:14:40 +0530

    adding codes

---
+++ c-language-practice/Day 1/hello.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+
+    // write your name age and college name in the output screen 
+
+    printf("Gohil Rajendrasinh\n");
+    printf("Age : 22\n");
+    printf("Swami sahajandand college of commerce and management\n");
+
+    // WP in c language to print 
+
+    printf("|-------|\n");
+    printf("|\t|\n");
+    printf("R\t|\n");
+    printf("N\t|\n");
+    printf("W\t|\n");
+    printf("R\t|\n");
+    printf("|-------|\n");
+
+    printf("\n");
+
+    // print the pattern 
+
+    printf("*\n");
+    printf("* *\n");
+    printf("* * *\n");
+    printf("* *\n");
+    printf("*\n");
+
+    return 0;
+
+
+
+}
